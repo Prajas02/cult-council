963f0a646f227a604aafbe4e28ae4fff78f804eb
jsmaskeen <jsmaskeen@gmail.com>
2024-07-26 12:44:07 +0530

    add backend base and vercel hosting

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+### Backend (Jaskirat and Naveen)
+
+1. Will be deployed on vercel.
+2. Requires mongodb credentials. [edit _config.py, add credentials, rename it to config.py]
+
+
+@anmolkumr, hosting this rn on my account's vercel, we will host on official later.
---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request,session,redirect,url_for
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def index():
+  return "Why are we here? Just to suffer !"
+
+
+
+@app.errorhandler(404)
+@cross_origin()
+def page_not_found(e):
+    return "Not found bbye", 404
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
