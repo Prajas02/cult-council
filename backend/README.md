4b2589f8da337246afca823df08bf7ba7a8d1d00
jsmaskeen <jsmaskeen@gmail.com>
2024-07-26 16:05:55 +0530

    add gsheet integration

--- backend/README.md
+++ backend/README.md
@@ -3,6 +3,7 @@
 1. Will be deployed on vercel.
 2. Requires mongodb credentials. [make a file .env and add there]
 3. [https://cult-council-api.vercel.app/](https://cult-council-api.vercel.app/)
+4. Google sheet of details: [link](https://docs.google.com/spreadsheets/d/1Io6E2XwiO0orqcO0_MYqBU9CMZlGMj7cyKPdvLvvAsY/edit?gid=2062762726#gid=2062762726) [IITGN-viewer]
 
 
 @anmolkumr, hosting this rn on my account's vercel, we will host on official later.
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,28 @@
-from flask import Flask,render_template,request,session,redirect,url_for
+from flask import Flask, render_template, request, session, redirect, url_for,jsonify
 from flask_cors import CORS, cross_origin
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
+import helper.gsheet as gs
+from googleapiclient.errors import HttpError
+from uuid import uuid4
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
+app.secret_key = str(uuid4())
+sheet = gs.Gsheet()
 
 @app.route("/")
 @cross_origin()
 def index():
-  return "Why are we here? Just to suffer !"
+    return "Why are we here? Just to suffer !"
+
+
+@app.route("/clubs/<club_name>")
+@cross_origin()
+def clubs(club_name):
+    try:
+        cb = sheet.get_club_by_name(club_name).get_dict()
+    except HttpError:
+        cb = {'Bad':'Request'}
+    return jsonify(cb)
 
 
 
@@ -21,6 +32,5 @@ def page_not_found(e):
     return "Not found bbye", 404
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
