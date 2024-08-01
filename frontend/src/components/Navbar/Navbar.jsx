18c7b87de598150b1479dc630816492042ee0ae5
Prajas Kulkarni <zero20206@outlook.com>
2024-08-01 09:04:32 +0530

    added navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,18 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='nav-container'>
+      <div className="logo-container">
+        <img src="https://th.bing.com/th/id/OIP.hO9SYk78nB0g-O8Kj8BDjAAAAA?rs=1&pid=ImgDetMain" alt="Logo" />
+        <div className="council-name"> Cultural Council</div>
+      </div>
+      <div className="links-container">
+        <span className='link'><a href="/Home">Home</a></span>
+        <span className='link'><a href="/Clubs">Clubs</a></span>
+        <span className='link'><a href="/Events">Events</a></span>
+        <span className='link'><a href="">About Us</a></span>
+      </div>
+      
     </div>
   )
 }
