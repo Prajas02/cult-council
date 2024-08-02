80dad972858646aa687637d7c6f109506c1a5368
Prajas Kulkarni <zero20206@outlook.com>
2024-08-03 01:24:09 +0530

    Added Clubs page
    There are some things in animations which are acting as bugs we have to figure it out.

--- frontend/src/components/Clubs/ClubElem.jsx
+++ frontend/src/components/Clubs/ClubElem.jsx
@@ -1,32 +1,58 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion, useTransform} from 'framer-motion'
 import './Clubs.css'
 
-const rotateVariants = {
-    rotate: {
-        rotate: 360
-    }
+const cardVar = {
+    initial: { x: "-50vw" },
+    visible: { x: 0 },
+    exit: { x: "-50vw" }
 }
 
-function ClubElem({ clubImgSrc }) {
+const textVar = {
+    initial: { x: "100vw" },
+    visible: { x: 0 },
+    exit: { x: "100vw" }
+}
+
+function ClubElem({ scrollYProgress,index }) {
+
+    console.log(scrollYProgress)
+    const scaleA = useTransform(scrollYProgress , [index,index + 0.5] , [1,0.8])
+    const rotateA = useTransform(scrollYProgress , [index, index + 0.5] , [0 , -5])
+
+    const scaleB = useTransform(scrollYProgress , [index, index+0.5] , [0.8,1])
+    const rotateB = useTransform(scrollYProgress , [index, index+0.5] , [-5 , 0])
+
     return (
-        <>
+        <motion.div 
+            className='all-container'
+            style={
+                {scale: scrollYProgress < index ? scaleB : scaleA, 
+                rotate: scrollYProgress < index ? rotateA : rotateA
+            }}>
             <div className="container"></div>
 
             <div className="focus">
-                <div className="card-container">
-                    <motion.div className="cards card-1" variants={rotateVariants} animate='rotate' transition={{duration: 4 , repeat: Infinity , repeatDelay: 1.5}}></motion.div>
-                    <motion.div className="cards card-2" variants={rotateVariants} animate='rotate' transition={{duration: 4 , delay: 1 , repeat: Infinity , repeatDelay: 2.5}}></motion.div>
-                    <motion.div className="cards card-3" variants={rotateVariants} animate='rotate' transition={{duration: 4 , delay: 0.5 ,repeat: Infinity , repeatDelay: 2}}></motion.div>
-                    <motion.div className="cards card-4" variants={rotateVariants} animate='rotate' transition={{duration: 4 , delay: 1.5 , repeat: Infinity , repeatDelay: 3}}></motion.div>
-                </div>
-                
-                <div className="club-text">
-                    <div className="cTitle">XYZ</div>
-                    <div className="cDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nihil ipsam facilis suscipit tempora deserunt magni molestias, eos eum obcaecati sequi ullam fugit, adipisci libero repellat? Sequi dignissimos ea reprehenderit.</div>
-                    <div className="cSecy">Hello World</div>
-                </div>
+                <AnimatePresence>
+                    <motion.div
+                        className="card-container"
+                    >
+                        <motion.div className="cards card-1"></motion.div>
+                        <motion.div className="cards card-2"></motion.div>
+                        <motion.div className="cards card-3"></motion.div>
+                        <motion.div className="cards card-4"></motion.div>
+                    </motion.div>
+                </AnimatePresence>
+
+                <AnimatePresence>
+                    <motion.div
+                        className="club-text">
+                        <div className="cTitle">XYZ</div>
+                        <div className="cDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nihil ipsam facilis suscipit tempora deserunt magni molestias, eos eum obcaecati sequi ullam fugit, adipisci libero repellat? Sequi dignissimos ea reprehenderit.</div>
+                        <div className="cSecy">Hello World</div>
+                    </motion.div>
+                </AnimatePresence>
             </div>
-        </>
+        </motion.div>
     )
 }
 
--- frontend/src/components/Clubs/Clubs.jsx
+++ frontend/src/components/Clubs/Clubs.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
 import ClubElem from './ClubElem'
 import './Clubs.css'
 
 const Clubs = () => {
+
+  const container = useRef()
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end']
+  })
   return (
-    <>
-    <ClubElem clubImgSrc=''/>
-    </>
+    <div ref={container} style={{backgroundColor: 'black'}}>
+      <ClubElem index={0} scrollYProgress={scrollYProgress} />
+      <ClubElem index={1} scrollYProgress={scrollYProgress}/>
+    </div>
   )
 }
 
